1ecafea5db08609c98aa800cd65f51f80542e5b2
Lazare Kvirtia <lazarekvirtia@gmail.com>
2021-11-19 17:30:00 +0400

    get_craftsman_phone_by_user_id && get_craftsman_email_by_user_id

--- src/main/java/com/lazo/jc/app/user/repository/AppUserMinimalRepository.java
+++ src/main/java/com/lazo/jc/app/user/repository/AppUserMinimalRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * Created by Lazo on 2021-11-19
- * Created for <strong>Ministry of Internal Affairs</strong>
  */
 
 public interface AppUserMinimalRepository extends JpaRepository<AppUserMinimalDomain, Long>, JpaSpecificationExecutor<AppUserMinimalDomain> {
